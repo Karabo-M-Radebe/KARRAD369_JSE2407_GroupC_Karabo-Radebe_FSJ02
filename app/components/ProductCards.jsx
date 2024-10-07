0df70d6db5791affb1efc578bf8f45937b4e47f7
Karabo M. Radebe <156776468+Karabo-M-Radebe@users.noreply.github.com>
2024-10-07 10:42:21 +0200

    refactored all my code for the components structure

--- app/components/ProductCards.jsx
+++ app/components/ProductCards.jsx
@@ -1,26 +1,8 @@
-// import { useRouter } from "next/navigation";
-
-// const Products = ( ) => {
-//     const router = useRouter();
-
-//     fetch (`https://next-ecommerce-api.vercel.app/products?skip=${router}`, {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             skip: 10,
-//             limit: 50,
-//             sortBy: "fragrances"
-//         })
-//     })
-// }
-
-// export default Products();
-
 "use client";
 
 import { useState, useEffect } from 'react';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import "../styles/globals.css";
-import SkeletonCard from './SkeletonCard';
+import ProductCard from './ProductCard';
 import Sort from './Sort';
 import Filter from './Filter';
 import Pagination from './pagination';
@@ -29,130 +11,56 @@ import Error from './Error';
 import { fetchProducts, fetchCategories } from '../lib/api';
 import Head from 'next/head';
 
-/**
- * ProductCards component
- * 
- * @param {object} initialProducts - Initial products data
- * @param {number} currentPage - Current page number
- * 
- * @returns {JSX.Element} - ProductCards component
- */
+<Head>
+  <title>The Pantry - Browse Products</title>
+  <meta name="description" content="Browse our wide range of products." />
+  <meta name="keywords" content="shop, products, buy" />
+  <meta name="author" content="The Pantry" />
+  <meta property="og:title" content="The Pantry" />
+  <meta property="og:description" content="Your one-stop shop for all your daily essentials." />
+  <meta property="og:type" content="website" />
+</Head>
+
+
 const ProductCards = ({ initialProducts, currentPage, initialCategories }) => {
   const [products, setProducts] = useState(initialProducts || []);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [filteredProducts, setFilteredProducts] = useState(products)
+  const [filteredProducts, setFilteredProducts] = useState(products);
   const [currentPageState, setCurrentPageState] = useState(currentPage || 1);
   const [categories, setCategories] = useState(initialCategories || []);
-  const [searchTerm, setSearchTerm] = useState("") //State variable for search input
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
- // const pageParam = searchParams.get('page') || '1';
- // const page = parseInt(pageParam) - 1;
 
   const perPage = 20;
   const totalFiltered = filteredProducts.length;
   const totalPages = Math.ceil(totalFiltered / perPage);
-  //const categories = [...new Set(products.map(product => product.category))];
-
-  const fetchCategories = async () => {
-    try {
-      const response = await fetch('https://next-ecommerce-api.vercel.app/categories');
-      const data = await response.json();
-      setCategories(data);
-    } catch {
-      console.error('Failed to fetch categories');
-    }
-  };
-
-   /**
-   * Fetch products data
-   * 
-   * @async
-   * @returns {Promise<void>}
-   */
-   const fetchProducts = async (page) => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        `https://next-ecommerce-api.vercel.app/products?skip=${(page - 1) * perPage}&limit=${perPage}`
-      );
-      if (!response.ok) {
-        throw new Error('Failed to fetch products');
-      }
-      const data = await response.json();
-      setProducts(data);
-      setFilteredProducts(data);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   useEffect(() => {
-    fetchProducts(currentPageState);
-    if (categories.length === 0) {
-      fetchCategories();
-    }
+    const loadProductsAndCategories = async () => {
+      setLoading(true);
+      try {
+        const fetchedProducts = await fetchProducts(currentPageState, perPage);
+        setProducts(fetchedProducts);
+        setFilteredProducts(fetchedProducts);
+
+        if (!categories.length) {
+          const fetchedCategories = await fetchCategories();
+          setCategories(fetchedCategories);
+        }
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadProductsAndCategories();
   }, [currentPageState]);
-
-
-   // Update filteredProducts whenever products change
-   useEffect(() => {
-    setFilteredProducts(products); // This ensures filteredProducts is populated with products after the fetch.
-  }, [products]);
-
-  /**
-   * Handle next page click
-   * 
-   * @returns {void}
-   */
-  const handleNextPage = () => {
-    const nextPage = currentPageState + 1;
-    //window.history.pushState({}, '', `${pathname}?page=${nextPage}`);
-    if (nextPage <= totalPages) {
-      setCurrentPageState(nextPage);
-      updateQueryParams({ page: nextPage });
-    }
-  };
-
-  /**
-   * Handle previous page click
-   * 
-   * @returns {void}
-   */
-  const handlePreviousPage = () => {
-    const prevPage = currentPageState - 1;
-    if (prevPage >= 1) {
-      setCurrentPageState(prevPage);
-      updateQueryParams({page: prevPage})
-      //window.history.pushState({}, '', `${pathname}?page=${prevPage}`);
-    }
-  };
-
-  const handleSort = (order) => {
-    const sortedProducts = [...filteredProducts].sort((a,b) => {
-     return order === "asc" ? a.price - b.price : b.price - a.price;
-    })
-    setFilteredProducts(sortedProducts);
-    updateQueryParams({ sort: order });
-  }
-
-  const handleFilter = (category) => {
-    if (category) {
-      const filtered = products.filter((product) => product.category === category)
-      setFilteredProducts(filtered);
-      setCurrentPageState(1); // Reset pagination after filtering
-      updateQueryParams({ filter: category, page: 1 });
-    } else {
-      setFilteredProducts(products);
-      setCurrentPageState(1)
-      updateQueryParams({ filter: null});
-    }
-  }
 
   const handleSearch = () => {
     const filtered = products.filter((product) => {
@@ -161,23 +69,39 @@ const ProductCards = ({ initialProducts, currentPage, initialCategories }) => {
         product.title.toLowerCase().includes(searchLower) ||
         product.category.toLowerCase().includes(searchLower) ||
         (product.tags && product.tags.join(", ").toLowerCase().includes(searchLower))
-      )
-    })  
+      );
+    });
     setFilteredProducts(filtered);
-    setCurrentPageState(1); // Reset pagination after filtering
-    //updateQueryParams({ search: searchTerm, page: 1 });
+    setCurrentPageState(1); 
   };
 
-   // Reset filters and sorting
-   const resetFilters = () => {
+  const handleSort = (order) => {
+    setSortOrder(order);
+    const sortedProducts = [...filteredProducts].sort((a, b) =>
+      order === 'asc' ? a.price - b.price : b.price - a.price
+    );
+    setFilteredProducts(sortedProducts);
+    updateQueryParams({ sort: order });
+  };
+
+  const handleFilter = (category) => {
+    setSelectedCategory(category);
+    const filtered = products.filter((product) => 
+      category ? product.category === category : true
+    );
+    setFilteredProducts(filtered);
+    setCurrentPageState(1); // Reset to page 1 after filtering
+  };
+
+  const resetFilters = () => {
     setFilteredProducts(products);
     setCurrentPageState(1);
     setSearchTerm("");
-    updateQueryParams({ sort: null, filter: null, page: 1 });
+    setSortOrder("");
   };
 
-   // Helper function to update query params
-   const updateQueryParams = (params) => {
+  // Reflect changes in the URL query parameters
+  const updateQueryParams = (params) => {
     const newParams = new URLSearchParams(searchParams);
     Object.entries(params).forEach(([key, value]) => {
       if (value) {
@@ -190,23 +114,8 @@ const ProductCards = ({ initialProducts, currentPage, initialCategories }) => {
     router.push(newPath);
   };
 
-  if (loading) {
-    return (
-        <div className="container mx-auto py-8 bg-gray-100">
-        <h1 className="text-2xl font-bold mb-6 text-center">Loading Products...</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          {/* Show Skeleton Loaders while loading */}
-          {Array.from({ length: 8 }).map((_, index) => (
-            <SkeletonCard key={index} />
-          ))}
-        </div>
-      </div>
-);
-  }
-
-  if (error) {
-    return <div className="my-36 text-center text-3xl text-red-500">Oops, there seems to be an error buddy! {error}</div>;
-  }
+  if (loading) return <Loading />;
+  if (error) return <Error message={error} />;
 
   return (
     <div className="container mx-auto py-8 bg-gray-100">
@@ -223,109 +132,33 @@ const ProductCards = ({ initialProducts, currentPage, initialCategories }) => {
           <button onClick={handleSearch} className="bg-gray-800 text-white py-2 px-4 rounded w-24">Search</button>
         </div>
         <Sort onSort={handleSort}/>
-        <Filter onFilter={handleFilter} categories ={categories}/>
-        <button className="bg-gray-800 text-white py-2 px-4 rounded w-60 my-4" onClick={resetFilters}>Reset all filters</button>
+        <Filter onFilter={handleFilter} categories={categories} />
+        <button className="bg-gray-800 text-white py-2 px-4 rounded w-60" onClick={resetFilters}>
+          Reset all filters
+        </button>
       </div>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {filteredProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      <div className="flex justify-center space-x-4 mt-8">
-        <div>
-            <button onClick={handlePreviousPage} disabled={currentPageState === 1} className="group" >
-                <svg 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    height="32" width="32" 
-                    viewBox="0 0 512 512"
-                    className="group-hover:scale-110">
-                    <path 
-                    fill="#b6c1d2" 
-                    d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z"/>
-                </svg>
-            </button>
 
-        </div>
-        
-        <p>{currentPageState}</p>
-        <div>
-            <button onClick={handleNextPage} disabled={currentPageState === totalPages} className="group">
-                <svg 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    height="32" width="32" 
-                    viewBox="0 0 512 512"
-                    className="group-hover:scale-110">
-                    <path 
-                    fill="#b6c1d2" 
-                    d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"/>
-                </svg>
-            </button>
-        </div>
-        
-      </div>
-    </div>
-  );
-};
-
-/**
- * ProductCard component
- * 
- * @param {object} product - Product data
- * 
- * @returns {JSX.Element} - ProductCard component
- */
-const ProductCard = ({ product }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  /**
-   * Update current image index
-   * 
-   * @param {number} prevIndex - Previous image index
-   * 
-   * @returns {number} - New image index
-   */
-  useEffect(() => {
-    if (product.images.length > 1) {
-      const interval = setInterval(() => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % product.images.length);
-      }, 3000); // 3 seconds interval
-
-      return () => clearInterval(interval); // Cleanup interval on component unmount
-    }
-  }, [product.images.length]);
-
-  return (
-    <div className="bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition-shadow duration-300">
-      <div className="relative">
-        <img
-          src={product.images[currentImageIndex]}
-          alt={product.title}
-          className="h-48 w-full object-contain mb-4 rounded"
-        />
-        {product.images.length > 1 && (
-          <div className="absolute bottom-2 left-2 bg-black bg-opacity-50 text-white text-xs px-2 py-1 rounded">
-            {currentImageIndex + 1}/{product.images.length}
-          </div>
-        )}
-      </div>
-      <h2 className="text-lg font-semibold">{product.title}</h2>
-      <p className="text-gray-600">Category: {product.category}</p>
-      <p className="text-gray-800 font-bold mt-2">${product.price}</p>
-      <a href={`/product/${product.id}`}>
-        <button className="bg-gray-800 text-white py-2 px-4 rounded w-full hover:bg-gray-600 mt-4">
-          View Details
-        </button>
-      </a>
+      <Pagination
+        currentPage={currentPageState}
+        totalPages={totalPages}
+        handleNextPage={() => setCurrentPageState(currentPageState + 1)}
+        handlePreviousPage={() => setCurrentPageState(currentPageState - 1)}
+      />
     </div>
   );
 };
 
 /**
  * Get server-side props
- * 
+ *
  * @async
  * @param {object} context - Context object
- * 
  * @returns {Promise<object>} - Server-side props
  */
 // Server-side rendering (SSR) to fetch products for the initial page load
